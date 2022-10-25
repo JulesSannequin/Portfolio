560128f5f911a5002e5089d9ebc49d3e4bd4061d
Jules Sannequin <jules.sannequin@gmail.com>
2022-10-25 14:57:11 +0200

    made the creation page , almost finished still some style to do

--- src/Pages/Creations.js
+++ src/Pages/Creations.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "../Style/Creations.css";
+import deliveroo from "../Images/deliveroo.png";
+import spaceinvader from "../Images/space-invader.png";
+import vinted from "../Images/Vinted.png";
 
 const Creations = () => {
-  return <div className="main-creations">Creations</div>;
+  return (
+    <div className="main-creations">
+      <div className="creations-card">
+        <a href="https://vintedjulessannequin.netlify.app">
+          <div className="creations-card-1">
+            <img src={vinted} alt="vinted-reproduction" />
+            <div className="text">
+              vous voulez faire du tri dans vos placards?{" "}
+            </div>
+          </div>
+        </a>
+
+        <div className="creations-card-2">
+          <img src={deliveroo} alt="deliveroo-reproduction" />
+          <div className="text">
+            Vous n'avez rien a manger ? pas de problème venez ici!
+          </div>
+        </div>
+        <div className="creations-card-3">
+          <img src={spaceinvader} alt="little-space-invader-game" />
+          <div className="text">
+            Vous vous ennuyez ? venez a jouer a mon space invader
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Creations;
