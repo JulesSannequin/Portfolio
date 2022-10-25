3a5f5cb9b4a460f072bb515c1d3e2436cef08869
Jules Sannequin <jules.sannequin@gmail.com>
2022-10-25 23:20:45 +0200

    start the structure of the parcours page

--- src/Pages/Parcours.js
+++ src/Pages/Parcours.js
@@ -2,7 +2,21 @@ import React from "react";
 import "../Style/Parcours.css";
 
 const Parcours = () => {
-  return <div className="main-parcours">Parcours</div>;
+  return (
+    <div className="main-parcours">
+      <div className="parcours-scroll">
+        <div className="parcours-card-1">card-1</div>
+        <div className="parcours-card-2">card-2</div>
+        <div className="parcours-card-3">card-3</div>
+        <div className="parcours-card-4">card-4</div>
+      </div>
+      <div className="parcours-scroll-bis">
+        <div className="parcours-card-5"></div>
+        <div className="parcours-card-6"></div>
+        <div className="parcours-card-7"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Parcours;
