52430e87138bce713cdefc54443a1e782d426bc3
Jules Sannequin <jules.sannequin@gmail.com>
2022-10-26 20:05:14 +0200

    finished the structure and the design of the Parcours's page

--- src/Pages/Parcours.js
+++ src/Pages/Parcours.js
@@ -5,15 +5,68 @@ const Parcours = () => {
   return (
     <div className="main-parcours">
       <div className="parcours-scroll">
-        <div className="parcours-card-1">card-1</div>
-        <div className="parcours-card-2">card-2</div>
-        <div className="parcours-card-3">card-3</div>
-        <div className="parcours-card-4">card-4</div>
+        <div className="parcours-card-1">
+          <p>
+            Passionné d'automobile c'est donc dans ce domaine que j'ai commencé
+            ma carrière
+          </p>
+        </div>
+        <div className="parcours-card-2">
+          <p>
+            Mon premier emploi a donc été chauffeur commercial pour la société
+            Aurige Services, j'étais chauffeur privé et je m'occupais de
+            chercher des contrats pour l'entreprise en parallèle
+          </p>
+        </div>
+        <div className="parcours-card-3">
+          <p>
+            J'ai par la suite monté mon entreprise : GS Covering avec mon
+            associé, nous avons donc géré cette société, nous vendions de
+            préstations de covering et différents elements de préparation
+            automobile et moto
+          </p>
+        </div>
+        <div className="parcours-card-4">
+          <p>
+            je suis par la suite rentré chez Volkswagen en tant que conseiller
+            client après-vente, je m'occupais de la clientèle et des véhicules
+            en après-vente et je manageais les mécaniciens
+          </p>
+        </div>
       </div>
       <div className="parcours-scroll-bis">
-        <div className="parcours-card-5"></div>
-        <div className="parcours-card-6"></div>
-        <div className="parcours-card-7"></div>
+        <div className="parcours-card-5">
+          <p>
+            C'est après cette éxperience que je sentais le besoin de changement
+            et l'envie d'aller vers des metiers d'avenir selon moi. étant
+            passionné d'informatique je me suis donc tourné vers le
+            developpement web!, de plus mon activité annexe (beatmaker,
+            compositeur) m'a appris a être créatif et a etre perseverant! me
+            voila donc déterminé pour atteindre mon objectif : devenir
+            développeur{" "}
+          </p>
+        </div>
+        <div className="parcours-card-6">
+          <p>
+            J'ai commencé avec la formation Open Classrooms ou j'ai appris
+            l'intégration html et css. Mes premiers pas dans le code je me
+            sentais épanoui mais j'avais envi de plus , j'ai donc commencé a
+            étudier le Javascript. En me renseigant j'ai trouvé ce qui allait
+            etre ma deuxieme experience: Le Reacteur
+          </p>
+        </div>
+        <div className="parcours-card-7">
+          <p>
+            Je voyais bien en fouillant sur les sites d'offre d'emploi que le
+            React était vraiement demandé , le reacteur a donc retenu mon
+            attention! De plus je voulais me tourner vers une formation full
+            stack. j'y ai donc appris le Javacript , le React , l'utilisation
+            des bases de données , des serveurs avec Express , l'utilisation des
+            API, c'était vraiment complet et intense! me voila donc a la
+            recherche d'un stage afin de pouvoir perfectionner mes
+            connaissances!
+          </p>
+        </div>
       </div>
     </div>
   );
