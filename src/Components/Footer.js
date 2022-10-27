c71166150ae01636e7ca401258eb596d0c8cc434
Jules Sannequin <jules.sannequin@gmail.com>
2022-10-27 03:30:44 +0200

    done the responsive for laptop and ipad air

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,12 @@ import React from "react";
 import "../Style/Footer.css";
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      Actuellement en recherche d'un stage de 6 mois , toutes les infos pour
+      pouvoir me joindre sur la page contact
+    </div>
+  );
 };
 
 export default Footer;
